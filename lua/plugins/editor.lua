53da00bde5bd8af7f07b966bccb8cae077596588
Brian Rodriguez <brian@brianrodri.com>
2026-09-01 16:22:26 -0400

    feat: setup "surround" operators

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,12 @@
 return {
     { 'tpope/vim-unimpaired' },
     { 'tpope/vim-sleuth' },
+
+    {
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
+        ---@module "nvim-surround"
+        ---@type user_options
+        opts = { move_cursor = "sticky" },
+    },
 }
