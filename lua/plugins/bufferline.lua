55b4622bcdb5799d5d53ff7a70d650ff4878a22b
Brian Rodriguez <brian@brianrodri.com>
2026-09-14 01:17:49 +0000

    feat: adapt color when modified or readonly

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,10 +5,14 @@ end
 local function runtime_choice(opts)
   return function(buf)
     local choice = opts[1]
-    if opts.error and buf.diagnostics.errors > 0 then
+    if opts.error and buf.diagnostics.errors > 0 and vim.diagnostic.is_enabled() then
       choice = opts.error
-    elseif opts.warning and buf.diagnostics.warnings > 0 then
+    elseif opts.warning and buf.diagnostics.warnings > 0 and vim.diagnostic.is_enabled() then
       choice = opts.warning
+    elseif opts.readonly and buf.is_readonly then
+      choice = opts.readonly
+    elseif opts.modified and buf.is_modified then
+      choice = opts.modified
     elseif opts.focused and buf.is_focused then
       choice = opts.focused
     end
@@ -30,6 +34,7 @@ return {
         unfocused = hlgroups.get_hl_attr("Comment", "fg"),
         error = hlgroups.get_hl_attr("DiagnosticError", "fg"),
         warning = hlgroups.get_hl_attr("DiagnosticWarn", "fg"),
+        readonly = hlgroups.get_hl_attr("DiagnosticInfo", "fg"),
       }
       local bg = {
         focused = hlgroups.get_hl_attr("ColorColumn", "bg"),
@@ -55,12 +60,16 @@ return {
                 function(buf) return buf.devicon.icon end,
                 error = my_icons.diagnostics.error .. " ",
                 warning = my_icons.diagnostics.warn .. " ",
+                modified = my_icons.modified .. " ",
+                readonly = my_icons.readonly .. " ",
               })
             ),
             fg = runtime_choice({
               function(buf) return buf.devicon.color end,
               error = fg.error,
               warning = fg.warning,
+              modified = fg.warning,
+              readonly = fg.readonly,
             }),
           },
           {
@@ -71,15 +80,16 @@ return {
           },
           {
             text = function(buf) return buf.filename end,
-            fg = runtime_choice({ error = fg.error, warning = fg.warning }),
+            fg = runtime_choice({
+              error = fg.error,
+              warning = fg.warning,
+              modified = fg.warning,
+              readonly = fg.readonly,
+            }),
             truncation = { direction = "left" },
           },
           {
-            text = runtime_format(" %s ", function(buf)
-              if buf.is_readonly then return my_icons.readonly end
-              if buf.is_modified then return my_icons.modified end
-              return my_icons.close
-            end),
+            text = string.format(" %s ", my_icons.close),
             on_click = function(_, _, _, _, buf) buf:delete() end,
           },
           {
